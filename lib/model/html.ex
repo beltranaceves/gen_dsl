406c1f4c6c0645c388b99872735f9f45392c160c
Beltrán Aceves <beltran.aceves@udc.es>
2024-01-18 02:03:51 +0100

    Ported command string generation to Html

--- lib/model/html.ex
+++ lib/model/html.ex
@@ -5,8 +5,8 @@ defmodule GenDSL.Model.Html do
   schema "Html" do
     field(:context, :string)
     field(:web, :string)
-    field(:no_context, :boolean)
-    field(:no_schema, :boolean)
+    field(:no_context, :boolean, default: false)
+    field(:no_schema, :boolean, default: false)
     field(:context_app, :string)
 
     # {name, table, fields, flags} # TODO: Check values with changeset for valid datatypes in fields
@@ -17,15 +17,19 @@ defmodule GenDSL.Model.Html do
     # TODO: check XOR schema/no_schema with constraints
   end
 
-  @required_fields ~w[]a
-  @optional_fields ~w[]a
+  @required_fields ~w[context]a
+  @optional_fields ~w[web context_app no_context no_schema]a
   # TODO: revise list when a method to XOR fields is introduced
-  @remainder_fields ~w[context web no_context no_schema context_app]a
+  @remainder_fields ~w[]a
+
+  @flags ~w[no_context no_schema]a
+  @named_arguments ~w[web context_app]a
+  @positional_arguments ~w[context]a
 
   def changeset(params \\ %{}) do
     %__MODULE__{}
     |> cast(params, @required_fields ++ @optional_fields ++ @remainder_fields, required: false)
-    |> cast_embed(:schema, required: false, with: &GenDSL.Model.Schema.changeset/1)
+    |> cast_embed(:schema, required: false, with: &GenDSL.Model.Schema.embedded_changeset/2)
     |> validate_required(@required_fields)
   end
 
@@ -48,6 +52,13 @@ defmodule GenDSL.Model.Html do
   def execute(html) do
     specs = []
 
-    Mix.Task.run(html.command, specs)
+    [valid_positional_arguments, valid_flags, valid_named_arguments, _valid_html] =
+      GenDSL.Model.get_valid_model!(html, @positional_arguments, @flags, @named_arguments)
+
+    valid_schema_spec = GenDSL.Model.Schema.to_valid_spec(html.schema)
+
+    specs = (specs ++ valid_positional_arguments ++ valid_schema_spec ++ valid_named_arguments ++ valid_flags) |> List.flatten()
+    IO.inspect(specs)
+    # Mix.Task.run("phx." <> html.command, specs)
   end
 end
